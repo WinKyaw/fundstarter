ebb737525b06d2afb1fe1715700ba908cc079ac3
Kyaw <skyversal@gmail.com>
2016-03-17 20:49:07 -0700

    make it better

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ var http = require("http");
 var fs = require("fs");
 
 //part 1(a)
-var readfilesync = function(callback){
-	return readfile('./index.html', 'utf8');
-}
+// var readfilesync = function(callback){
+// 	return readfile('./index.html', 'utf8');
+// }
 
-var connection = function(request, response){
+// var connection = function(request, response){
 	
 
-	return response.sendreadSyncHTML();
-}
+// 	return response.sendreadSyncHTML();
+// }
 
-var server = http.createServer(connection);
-server.listen(8100);
+// var server = http.createServer(connection);
+// server.listen(8100);
+
+
+//part 1(b)
+fs.readFile('./index.html', function(err, html) {
+	if(err) {
+		throw err;
+	}
+	
+	var server = http.createServer(function(request, response) {
+		response.writeHeader(200, {"Content-Type": "text/html"});
+		response.write(html);
+		response.end();
+	});
+	server.listen(8000);
+});
 
